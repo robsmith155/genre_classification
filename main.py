b29bf23e1af3de0332dd1338f00cbc633f5f0a92
Rob Smith <robsmith155@gmail.com>
2021-12-03 06:34:22 +0300

    update main.py

--- main.py
+++ main.py
@@ -20,37 +20,65 @@ def go(config: DictConfig):
         # This was passed on the command line as a comma-separated list of steps
         steps_to_execute = config["main"]["execute_steps"].split(",")
     else:
-        assert isinstance(config["main"]["execute_steps"], list)
-        steps_to_execute = config["main"]["execute_steps"]
+        #assert isinstance(config["main"]["execute_steps"], list)
+        steps_to_execute = list(config["main"]["execute_steps"])
 
     # Download step
     if "download" in steps_to_execute:
 
         _ = mlflow.run(
-            os.path.join(root_path, "download"),
-            "main",
+            uri=os.path.join(root_path, "download"),
+            entry_point="main",
             parameters={
                 "file_url": config["data"]["file_url"],
                 "artifact_name": "raw_data.parquet",
                 "artifact_type": "raw_data",
                 "artifact_description": "Data as downloaded"
-            },
+            }
         )
 
     if "preprocess" in steps_to_execute:
 
         ## YOUR CODE HERE: call the preprocess step
-        pass
+        _ = mlflow.run(
+                uri=os.path.join(root_path, "preprocess"),
+                entry_point="main",
+                parameters={
+                    "input_artifact": "raw_data.parquet:latest",
+                    "artifact_name": "preprocessed_data.csv",
+                    "artifact_type": "preprocessed_data",
+                    "artifact_description": "Data after preprocessing"
+                }
+        )
 
     if "check_data" in steps_to_execute:
 
         ## YOUR CODE HERE: call the check_data step
-        pass
+        _ = mlflow.run(
+                uri=os.path.join(root_path, "check_data"),
+                entry_point="main",
+                parameters={
+                    "reference_artifact": config['data']['reference_dataset'],
+                    "sample_artifact": "preprocessed_data.csv:latest",
+                    "ks_alpha": config['data']['ks_alpha']
+                }
+        )
 
     if "segregate" in steps_to_execute:
 
         ## YOUR CODE HERE: call the segregate step
-        pass
+        _ = mlflow.run(
+                uri=os.path.join(root_path, "segregate"),
+                entry_point="main",
+                parameters={
+                    "input_artifact":"preprocessed_data.csv:latest",
+                    "artifact_root":"data",
+                    "artifact_type":"segregated_data",
+                    "test_size":config['data']['test_size'],
+                    "random_state":config['main']['random_seed'],
+                    "stratify":config['data']['stratify']
+                }
+        )
 
     if "random_forest" in steps_to_execute:
 
@@ -61,12 +89,30 @@ def go(config: DictConfig):
             fp.write(OmegaConf.to_yaml(config["random_forest_pipeline"]))
 
         ## YOUR CODE HERE: call the random_forest step
-        pass
+        _ = mlflow.run(
+                uri=os.path.join(root_path,"random_forest"),
+                entry_point="main",
+                parameters={
+                    "train_data":"data_train.csv:latest",
+                    "model_config":model_config,
+                    "export_artifact":config['random_forest_pipeline']['export_artifact'],
+                    "random_seed":config['main']['random_seed'],
+                    "val_size":config['data']['val_size'],
+                    "stratify":config['data']['stratify']
+                }
+        )
 
     if "evaluate" in steps_to_execute:
 
         ## YOUR CODE HERE: call the evaluate step
-        pass
+        _ = mlflow.run(
+                uri=os.path.join(root_path,"evaluate"),
+                entry_point="main",
+                parameters={
+                    "model_export":f"{config['random_forest_pipeline']['export_artifact']}:latest",
+                    "test_data":"data_test.csv:latest"
+                }
+        )
 
 
 if __name__ == "__main__":
